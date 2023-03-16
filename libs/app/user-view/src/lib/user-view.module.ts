0f98c1bd56d25fa1ff3a5e5b7f9dcfbc4fb4d45a
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-16 13:23:27 +0200

    Added Ionic Module and header for UserViewPage

--- libs/app/user-view/src/lib/user-view.module.ts
+++ libs/app/user-view/src/lib/user-view.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserViewPageComponent } from './user-view.page';
 import { UserViewRouting } from './user-view.routing';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   imports: [
     CommonModule,
-    UserViewRouting
+    UserViewRouting,
+    IonicModule
   ],
   declarations: [UserViewPageComponent],
 })
