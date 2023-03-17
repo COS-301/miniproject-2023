925f9c4c7e0512bc5c9426e5a023dc2075363af6
Adrian-Hawkins <u21669831@tuks.co.za>
2023-03-17 09:13:10 +0200

    Removed navbar component from core

--- libs/app/core/feature/src/core.module.ts
+++ libs/app/core/feature/src/core.module.ts
@@ -50,7 +50,6 @@ import { NgxsModule } from '@ngxs/store';
 import { MomentModule } from 'ngx-moment';
 import { CoreRouting } from './core.routing';
 import { CoreShell } from './core.shell';
-import { NavBarComponent, AppNavbarFeatureModule } from '@mp/app/navbar/feature';
 
 let resolvePersistenceEnabled: (enabled: boolean) => void;
 
@@ -90,7 +89,6 @@ const FIREBASE_OPTIONS: FirebaseOptions = {
     BrowserModule,
     IonicModule.forRoot(),
     CoreRouting,
-    AppNavbarFeatureModule,
     provideRemoteConfig(() => getRemoteConfig()),
     provideAnalytics(() => getAnalytics()),
     provideMessaging(() => getMessaging()),
@@ -161,6 +159,6 @@ const FIREBASE_OPTIONS: FirebaseOptions = {
     ErrorsModule,
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [CoreShell, NavBarComponent],
+  bootstrap: [CoreShell],
 })
 export class CoreModule {}
