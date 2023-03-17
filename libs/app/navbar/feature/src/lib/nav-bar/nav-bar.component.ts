5242cc16dce409c71946be560370f4422657f39f
Adrian-Hawkins <u21669831@tuks.co.za>
2023-03-17 16:58:09 +0200

    Fixed navbar hopefully

--- libs/app/navbar/feature/src/lib/nav-bar/nav-bar.component.ts
+++ libs/app/navbar/feature/src/lib/nav-bar/nav-bar.component.ts
@@ -10,6 +10,7 @@ export class NavBarComponent {
   showNavbar!: boolean;
   constructor(private router: Router) {
     //These routes will have to be finalised once we have all the pages
+    //this.showNavbar = true;
     this.showNavbar = this.hasRoute('/home') || this.hasRoute('/messages') || this.hasRoute('/profile') || this.hasRoute('/notifications') || this.hasRoute('/post');
   }
   hasRoute(route: string) {
