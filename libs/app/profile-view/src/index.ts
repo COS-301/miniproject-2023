8599d7757557980125b5d3d09eaeff413f7c1012
08Arno30 <u21457451@tuks.co.za>
2023-03-19 19:32:02 +0200

    Fixed new component's import

--- libs/app/profile-view/src/index.ts
+++ libs/app/profile-view/src/index.ts
@@ -3,3 +3,5 @@ export * from './profile-view.module';
 export * from './profile-view.page';
 
 export * from './lib/add-memory/add-memory.page';
+
+export * from './lib/edit-profile-photo/edit-profile-photo.page';
--- libs/app/profile-view/src/profile-view.module.ts
+++ libs/app/profile-view/src/profile-view.module.ts
@@ -5,6 +5,7 @@ import { ProfileViewPageComponent } from './profile-view.page';
 import { ProfileViewRouting } from './profile-view.routing';
 import { AddMemoryPageComponent } from './lib/add-memory/add-memory.page';
 import { FormsModule } from '@angular/forms';
+import { EditProfilePhotoPageComponent } from './lib/edit-profile-photo/edit-profile-photo.page';
 
 @NgModule({
   imports: [CommonModule, ProfileViewRouting, IonicModule, FormsModule],
@@ -12,7 +13,12 @@ import { FormsModule } from '@angular/forms';
     ProfileViewPageComponent,
     AddMemoryPageComponent,
     AddMemoryPageComponent,
+    EditProfilePhotoPageComponent,
   ],
-  exports: [ProfileViewPageComponent, AddMemoryPageComponent,],
+  exports: [
+    ProfileViewPageComponent,
+    AddMemoryPageComponent,
+    EditProfilePhotoPageComponent,
+  ],
 })
 export class ProfileViewModule {}
