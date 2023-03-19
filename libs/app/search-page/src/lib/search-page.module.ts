15ddaeb8b3578c4d999251f6fbc5567b12fd5b3e
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-19 20:13:00 +0200

    Fixed search-page not rendering

--- libs/app/search-page/src/lib/search-page.module.ts
+++ libs/app/search-page/src/lib/search-page.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SearchPageComponent } from './search.page';
+import { SearchPageRouting } from './search-page.routing';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    SearchPageRouting,
+    IonicModule
+  ],
+  declarations: [SearchPageComponent],
 })
 export class SearchPageModule {}
