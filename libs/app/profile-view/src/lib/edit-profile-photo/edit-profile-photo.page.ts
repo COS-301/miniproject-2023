d8ab82f1e89929bef06523302098ccf731ddeb00
08Arno30 <u21457451@tuks.co.za>
2023-03-19 19:29:16 +0200

    Added new component: edit profile photo

---
+++ libs/app/profile-view/src/lib/edit-profile-photo/edit-profile-photo.page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-profile-photo',
+  templateUrl: './edit-profile-photo.page.html',
+  styleUrls: ['./edit-profile-photo.page.scss'],
+})
+export class EditProfilePhotoPageComponent {}
