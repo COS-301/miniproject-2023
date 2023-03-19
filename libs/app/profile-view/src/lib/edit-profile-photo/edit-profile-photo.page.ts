97d94594e18e346bcec889c4ac2e7710d659e216
08Arno30 <u21457451@tuks.co.za>
2023-03-19 19:42:33 +0200

    Added functions for click events

--- libs/app/profile-view/src/lib/edit-profile-photo/edit-profile-photo.page.ts
+++ libs/app/profile-view/src/lib/edit-profile-photo/edit-profile-photo.page.ts
@@ -1,8 +1,35 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-profile-photo',
   templateUrl: './edit-profile-photo.page.html',
   styleUrls: ['./edit-profile-photo.page.scss'],
 })
-export class EditProfilePhotoPageComponent {}
+export class EditProfilePhotoPageComponent {
+
+  profileImageUrl: string;
+
+  constructor(private modalController: ModalController) {
+    this.profileImageUrl = '';
+  }
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      if (reader.result !== null) {
+        this.profileImageUrl = reader.result.toString();
+      }
+    };
+  }
+
+  cancel() {
+    this.modalController.dismiss();
+  }
+
+  save() {
+    this.modalController.dismiss();
+  }
+}
