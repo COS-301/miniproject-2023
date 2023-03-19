286d326ea2cf5e18d492979818a3f9de4e5dade3
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-19 23:01:17 +0200

    Added search functionality

--- libs/app/search-page/src/lib/search.page.ts
+++ libs/app/search-page/src/lib/search.page.ts
@@ -6,13 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPageComponent {  
-  searchValue: string | undefined;
+  searchValue = '';
   searchFocus = false;
+  recentSearches: string[] = ['1','2','3','4','5']; //first 7 recents are shown
+  searchUsers: string[] = [
+    '@user1',
+    '@user2exa',
+    '@user3',
+    '@user4',
+    '@user5example',
+    '@user6',
+    '@user7test',
+    '@user8',
+    '@user9sport'];
 
   onSearchFocus() {
     this.searchFocus = true;
   }
   onSearchBlur() {
     this.searchFocus = false;
+  }
+  onSearch(searchTerm: string) {
+    // Add search term to the beginning of the array
+    if(searchTerm != '') this.recentSearches.unshift(searchTerm);
+    //fetch user accounts based on search value and populate searchUsers array
+  }  
+
+  get RecentSearches() {
+    return this.recentSearches;
+  }
+  get SearchedUsers() {
+    return this.searchUsers.filter(user => {
+      return user.toLowerCase().includes(this.searchValue.toLowerCase());
+    });
   }
 }
