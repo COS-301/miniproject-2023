b2d90f8075bbf44ab215995e457b426bbeeb3739
08Arno30 <u21457451@tuks.co.za>
2023-03-19 18:25:00 +0200

    Deleted revive-memory component

--- libs/app/profile-view/src/lib/revive-memory/revive-memory.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-revive-memory',
-  templateUrl: './revive-memory.page.html',
-  styleUrls: ['./revive-memory.page.scss'],
-})
-export class ReviveMemoryPage {}
