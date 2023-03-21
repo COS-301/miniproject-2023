cc61ad2f6e3fa23170dd2c74dae81fcba0f0d91b
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-21 18:52:31 +0200

    Updated HTML to have dynamic background image

--- libs/app/search-results/src/lib/search-results.page.ts
+++ libs/app/search-results/src/lib/search-results.page.ts
@@ -14,7 +14,7 @@ export class SearchResultsPageComponent {
   memoriesArray: any[] = [
     {
       username: '@username',
-      imgUrl: "",
+      imgUrl: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
       title: "Last day of Highschool",
       description: "Example of a description for the memory",
       comments: [{}],
