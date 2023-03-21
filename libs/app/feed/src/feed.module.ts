d846db64fcc404df89edc7479bcc9d91115fe623
08Arno30 <u21457451@tuks.co.za>
2023-03-21 21:31:31 +0200

    Added feed component to declarations and exports

--- libs/app/feed/src/feed.module.ts
+++ libs/app/feed/src/feed.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FeedPageComponent } from './feed.page';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    FeedPageComponent,
+  ],
+  exports: [
+    FeedPageComponent,
+  ]
 })
 export class FeedModule {}
