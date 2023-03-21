2c3d8980c39c27219587b7473ad72b2798ea0515
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-21 23:18:14 +0200

    Added dynamic data for comment section

--- libs/app/search-results/src/lib/search-results.page.ts
+++ libs/app/search-results/src/lib/search-results.page.ts
@@ -20,7 +20,13 @@ export class SearchResultsPageComponent {
       imgUrl: "https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aHVtYW58ZW58MHx8MHx8&w=1000&q=80",
       title: "Last day of Highschool",
       description: "Example of a description for the memory",
-      comments: [{}],
+      comments: [
+        {
+          username: '@commentedUsername',
+          profileImgUrl: 'https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000&q=60',
+          comment: 'This is an example comment. The idea of this comment is to show you what a comment on a memory looks like. And that it can overflow.' 
+        }
+      ],
       timePosted: '2020-11-14T10:30:00.000-07:00'
     }
   ];
