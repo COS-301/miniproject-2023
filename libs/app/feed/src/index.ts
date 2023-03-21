7ec14719f13f79a0dcbce85bf037644890e55087
08Arno30 <u21457451@tuks.co.za>
2023-03-21 21:14:04 +0200

    Removed library and component

--- libs/app/feed/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/app-feed.module';
-
-export * from './lib/lib.routes';
-
-export * from './lib/feed/feed-content.page';
--- libs/app/feed/src/lib/feed/feed-content.page.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-feed-content',
-  templateUrl: './feed-content.page.html',
-  styleUrls: ['./feed-content.page.scss'],
-})
-export class FeedContentPage {}
