e576b59324f2053a8a2f7a4a7b36b81e7707b903
08Arno30 <u21457451@tuks.co.za>
2023-03-21 23:10:14 +0200

    Modified feed page contents

--- libs/app/feed/src/feed.page.ts
+++ libs/app/feed/src/feed.page.ts
@@ -5,4 +5,20 @@ import { Component } from '@angular/core';
   templateUrl: './feed.page.html',
   styleUrls: ['./feed.page.scss'],
 })
-export class FeedPageComponent {}
+export class FeedPageComponent {
+
+  memoriesArray: any[] = [
+    {
+      username: '@username',
+      imgUrl: "",
+      title: "Last day of Highschool",
+      description: "Example of a description for the memory",
+      comments: [{}],
+      date: '14 November 2020'
+    }
+  ];
+
+  get Memories() {
+    return this.memoriesArray;
+  }
+}
