e2b640f2235d26be548797d5b27dd78074c57c41
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-21 19:51:49 +0200

    Added dynamic calculation for how long ago.

--- libs/app/search-results/src/lib/search-results.page.ts
+++ libs/app/search-results/src/lib/search-results.page.ts
@@ -80,6 +80,38 @@ export class SearchResultsPageComponent {
     const date = new Date(timePosted);
     return formatDate(date, 'dd MMMM yyyy', 'en-US');
   }
+  //function to use timePosted to calculate how long ago the memory was posted
+  calculateHowLongAgo(timePosted: string) : string {
+    const date = new Date(timePosted);
+    const timeDifference = Date.now() - date.getTime();
+
+    // Convert time difference to "time ago" string
+    const seconds = Math.floor(timeDifference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const weeks = Math.floor(days / 7);
+    const years = Math.floor(days / 365);
+
+    if (years > 0) {
+      return `${years} year${years > 1 ? 's' : ''} ago`;
+    } 
+    else if (weeks > 0) {
+      return `${weeks} week${weeks > 1 ? 's' : ''} ago`;
+    } 
+    else if (days > 0) {
+      return `${days} day${days > 1 ? 's' : ''} ago`;
+    } 
+    else if (hours > 0) {
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } 
+    else if (minutes > 0) {
+      return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    } 
+    else {
+      return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+    }
+  }
 
   //filter search results
   setFilter(filter: string) {
