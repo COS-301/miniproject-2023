60d6f555cb6b96dae6deb2e863c9df99d30cbe20
U18034332 <U18034332@tuks.co.za>
2023-03-22 19:26:10 +0200

    Deleted the unnecessary generated files

--- libs/app/footer/feature/src/footer.component.ts
+++ libs/app/footer/feature/src/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mp-footer',
+  selector: 'ms-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
