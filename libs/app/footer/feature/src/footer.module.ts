2ef5e0a6647a09cf69a6902c611dabfadd49ced6
U18034332 <U18034332@tuks.co.za>
2023-03-23 13:47:40 +0200

    Added routes

--- libs/app/footer/feature/src/footer.module.ts
+++ libs/app/footer/feature/src/footer.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { FooterComponent } from './footer.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [FooterComponent],
-  imports: [CommonModule, IonicModule],
+  imports: [CommonModule, IonicModule, RouterModule],
   exports: [FooterComponent],
 })
 export class FooterModule {}
