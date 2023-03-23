581a5243d6e3b6e589cc0eb81aec304f7c492814
Thapelo64 <68476683+Thapelo64@users.noreply.github.com>
2023-03-23 09:46:00 +0200

    loading page
    Implementation of the loading page

---
+++ libs/app/loading/feature/src/lib/app-loading-feature.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LoadingScreenComponent],
+})
+export class AppLoadingFeatureModule {}
