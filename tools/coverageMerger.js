1408caef1411feaa0a8f5aa6ea1411ddd21b6bfe
Ryan Trickett <64923970+ryanbasiltrickett@users.noreply.github.com>
2023-03-24 11:35:07 +0000

    👷 (workflows) Fixed Code Coverage File Creation

--- tools/coverageMerger.js
+++ tools/coverageMerger.js
@@ -2,17 +2,8 @@ const glob = require('glob');
 const fs = require('fs');
 const path = require('path');
 
-const getLcovFiles = function (src) {
-  return new Promise((resolve, reject) => {
-    glob(`${src}/**/lcov.info`, (error, result) => {
-      if (error) return reject(error);
-      resolve(result);
-    });
-  })
-};
-
 (async function(){
-  const files = await getLcovFiles('coverage');
+  const files = glob.sync('coverage/**/lcov.info');
   const mergedReport = files.reduce((mergedReport, currFile) => mergedReport += fs.readFileSync(currFile), '');
   await fs.writeFile(path.resolve('./coverage/lcov.info'), mergedReport, (err) => {
     if (err) throw err;
