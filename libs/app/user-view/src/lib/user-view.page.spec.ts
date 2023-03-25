2657cc39ca8e37bdbd55c987d85f9fe4e3110f4c
JsteReubsSoftware <joostereuben0830@gmail.com>
2023-03-25 12:03:35 +0200

    Added unit tests for user-view page

--- libs/app/user-view/src/lib/user-view.page.spec.ts
+++ libs/app/user-view/src/lib/user-view.page.spec.ts
@@ -1,22 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UserViewPage } from './user-view.page';
+import { UserViewPageComponent } from './user-view.page';
 
-describe('UserViewPage', () => {
-  let component: UserViewPage;
-  let fixture: ComponentFixture<UserViewPage>;
+describe('UserViewPageComponent', () => {
+  let component: UserViewPageComponent;
+  let fixture: ComponentFixture<UserViewPageComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [UserViewPage],
+      declarations: [UserViewPageComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(UserViewPage);
+    fixture = TestBed.createComponent(UserViewPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change button content to "You are friends" if the "Add friend" button is clicked and should set the "added" variable to true', () => {
+    component.addedNewFriend();
+
+    expect(component.btn_text).toEqual('You are friends');
+    expect(component.added).toBeTruthy();
+  });
+
+  it('should change button content to "Send friend request" if the "Unfriend" button is clicked and should set the "added" variable to false', () => {
+    component.removeFriend();
+
+    expect(component.btn_text).toEqual('Send friend request');
+    expect(component.added).toBeFalsy();
   });
 });
