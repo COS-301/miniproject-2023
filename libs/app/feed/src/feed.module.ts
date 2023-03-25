a3f4137c006d14688c6feba2795fcf895c100aa9
08Arno30 <u21457451@tuks.co.za>
2023-03-25 09:04:46 +0200

    Added SharedModule import

--- libs/app/feed/src/feed.module.ts
+++ libs/app/feed/src/feed.module.ts
@@ -3,9 +3,10 @@ import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FeedPageComponent } from './feed.page';
 import { FeedRouting } from './feed.routing';
+import { SharedModule } from '@mp/app/shared';
 
 @NgModule({
-  imports: [CommonModule, IonicModule, FeedRouting],
+  imports: [CommonModule, IonicModule, FeedRouting, SharedModule],
   declarations: [
     FeedPageComponent,
   ],
