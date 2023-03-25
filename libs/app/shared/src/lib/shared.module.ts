d1f06d3a66c67e8ed9d9d4b55d8ba55b3cebd657
08Arno30 <u21457451@tuks.co.za>
2023-03-25 08:59:47 +0200

    Updated imports and added declaration

--- libs/app/shared/src/lib/shared.module.ts
+++ libs/app/shared/src/lib/shared.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { AddMemoryPageComponent } from './add-memory/add-memory.page';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, IonicModule, FormsModule],
+  declarations: [
+    AddMemoryPageComponent
+  ]
 })
 export class SharedModule {}
