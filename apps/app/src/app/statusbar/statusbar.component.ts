0a810dd4856c753d709f7bf90d2286bf72201fce
Rec1dite <rec1dite@gmail.com>
2023-03-25 19:23:41 +0000

    Expanded navbar, Started home page; Added trending section with basic styling

---
+++ apps/app/src/app/statusbar/statusbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ms-statusbar',
+  templateUrl: './statusbar.component.html',
+  styleUrls: ['./statusbar.component.sass'],
+})
+export class StatusbarComponent {}
--- libs/app/dashboard/feature/src/dashboard.page.ts
+++ libs/app/dashboard/feature/src/dashboard.page.ts
@@ -9,6 +9,30 @@ import { Observable } from 'rxjs';
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
+
 export class DashboardPage {
   @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
+
+  // A bunch of dummy trending posts
+  trending = [
+    { title: "I am a trending post", desc: "I am a trending post description", img: "https://picsum.photos/id/18/300/300"},
+    { title: "I am a trending post", desc: "I am a trending post description", img: "https://picsum.photos/id/19/300/300"},
+    { title: "I am a trending post", desc: "I am a trending post description", img: "https://picsum.photos/id/20/300/300"},
+    { title: "I am a trending post", desc: "I am a trending post description", img: "https://picsum.photos/id/21/300/300"},
+    { title: "I am a trending post", desc: "I am a trending post description", img: "https://picsum.photos/id/22/300/300"},
+    { title: "I am a trending post", desc: "I am a trending post description", img: "https://picsum.photos/id/23/300/300"},
+  ]
+
+  isSearchbarVisible = false;
+
+  toggleSearchbar() {
+    this.isSearchbarVisible = !this.isSearchbarVisible;
+  }
+
+  loadData(event: any) {
+    setTimeout(() => {
+      event.target.complete();
+      event.target.disabled = true;
+    }, 2000);
+  }
 }
