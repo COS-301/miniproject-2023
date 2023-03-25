6f942eb4398c6d9cfa13c66dc386e84159d45ea9
AmickeC <croucampa01@gmail.com>
2023-03-25 16:40:59 +0200

    made new page

---
+++ libs/app/interests/feature/src/index.ts
@@ -0,0 +1,4 @@
+
+export * from './app-interests-feature.module';
+export * from './app-interests-feature.routing';
+export * from './lib/interests.page';
---
+++ libs/app/interests/feature/src/lib/interests.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mp-interests',
+  templateUrl: './interests.page.html',
+  styleUrls: ['./interests.page.scss']
+})
+export class InterestsPage {
+
+}
