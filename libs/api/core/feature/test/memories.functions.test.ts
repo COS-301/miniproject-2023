e2c6ec2669c9671011da2d10016e288114d95c69
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-03-25 18:23:16 +0200

    dumy test files with jest

---
+++ libs/api/core/feature/test/memories.functions.test.ts
@@ -0,0 +1,29 @@
+import {describe, expect, test} from '@jest/globals';
+import { CreateMemory } from './../src/functions/memories.functions'
+import {MemoriesService} from '@mp/api/memories/feature';
+import {
+    ICreateMemoryRequest,
+	ICreateMemoryResponse
+} from '@mp/api/memories/util';
+import { IMemory } from './../../../memories/util/src/interfaces/memory.interface';
+
+
+describe('Tesing Cloud Function: memories.functions -- with a null memory',()=>{
+    test('Creating Memory',()=>{
+        const memory:IMemory={
+            userId:null,
+            displayName:null,
+            imgUrl:null, 
+            alive : null,
+            time:null,
+            comments: null
+        };
+        const createMemoryRequest:ICreateMemoryRequest={
+            memory:memory
+        }
+        const createMemoryResponse:ICreateMemoryRequest={
+            memory:memory
+        }
+        CreateMemory(createMemoryRequest,createMemoryResponse)
+    })
+})
---
+++ libs/api/memories/util/test/CreateMemoryCommand.test.ts
@@ -0,0 +1,11 @@
+import { CreateCommentCommand,CreateMemoryCommand } from './../src/commands';
+import {describe, expect, test} from '@jest/globals';
+
+describe('Testing commands: ', ()=>{
+    test('create Memory',()=>{
+        
+    });
+    test('create comment',()=>{
+    
+    });
+})
