367517a79854c902251bdaa2d7babc26771927ae
08Arno30 <u21457451@tuks.co.za>
2023-03-25 08:52:05 +0200

    Updated imports for modules

--- libs/app/add-memory/src/lib/add-memory.module.ts
+++ libs/app/add-memory/src/lib/add-memory.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '@mp/app/shared';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule, IonicModule, FormsModule],
+
 })
 export class AddMemoryModule {}
