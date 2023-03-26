b7a7310238088fe0305dfb98b4afe30d08a8c04d
GD Radloff <danielrad082@gmail.com>
2023-03-26 22:22:16 +0200

    Added Message Saga Skeleton and Message Module

--- libs/api/message/feature/src/index.ts
+++ libs/api/message/feature/src/index.ts
@@ -2,3 +2,5 @@ export * from './tests/api-message-feature';
 export * from './commands';
 export * from './events';
 export * from './message.service';
+export * from './message.sagas';
+export * from './message.module';
