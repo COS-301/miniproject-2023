d890a54da48ba37ef45286eda384c6a9b797e206
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2023-03-26 12:48:02 +0200

    creating Post model

---
+++ libs/api/post/feature/src/models/post.model.ts
@@ -0,0 +1,59 @@
+import { Timestamp } from 'firebase-admin/firestore';
+import { AggregateRoot } from '@nestjs/cqrs';
+import { Hashtag, IComment, IPost } from '@mp/api/postss/util';
+
+export class Post extends AggregateRoot implements IPost {
+    constructor(
+        public postID: string,
+        public createdBy: string,
+        public likes: number, //fixed like left out
+        public ownedBy: string | null | undefined,
+        public buyerID?: string| null,
+        public comments?: IComment[] | null,
+        public createdAt?: Timestamp | null | undefined,
+        public content?: string | null | undefined,
+        public hashtag?: Hashtag | null |undefined,
+        public caption? : string | null | undefined,
+        public totalTime? : number | null | undefined,
+        public ownerGainedTime?: number | null | undefined,
+        public listing? : number | null | undefined
+    ) {
+        super();
+    }
+
+    static fromData(post: IPost): Post {
+        const instance = new Post(      
+            post.postID,
+            post.createdBy,
+            post.likes,
+            post.buyerID,
+            post.ownedBy,
+            post.comments,
+            post.createdAt,
+            post.content,
+            post.hashtag,
+            post.caption,
+            post.totalTime,
+            post.ownerGainedTime,
+            post.listing,
+        );
+        return instance;
+      }
+
+
+      toJSON(): IPost {
+        return {
+          postID: this.postID,
+          createdBy: this.createdBy,
+          likes: this.likes,
+          ownedBy: this.ownedBy,
+          createdAt: this.createdAt,
+          content: this.content,
+          hashtag: this.hashtag,
+          caption: this.caption,
+          totalTime: this.totalTime,
+          ownerGainedTime: this.ownerGainedTime,
+          listing: this.listing,
+        };
+      }
+}
--- libs/api/postss/util/src/interfaces/post.interface.ts
+++ libs/api/postss/util/src/interfaces/post.interface.ts
@@ -1,19 +1,19 @@
 import { Timestamp } from 'firebase-admin/firestore';
 import { Hashtag } from '../enums';
-import {IComment} from './comment.interface';
+import { IComment } from './comment.interface';
 
 export interface IPost {
-  buyerID?: string| null;
-  postID: string;
-  createdBy: string;
-  ownedBy: string | null | undefined;
-  likes: number; //fixed like left out
-  comments?: IComment[] | null;
-  createdAt?: Timestamp | null | undefined;
-  content?: string | null | undefined;
-  hashtag?: Hashtag | null |undefined;
-  caption? : string | null | undefined;
-  totalTime? : number | null | undefined
-  ownerGainedTime?: number | null | undefined;
-  listing? : number | null | undefined;
+   postID: string;
+   createdBy: string;
+   likes: number; //fixed like left out
+   ownedBy: string | null | undefined;
+   buyerID?: string| null;
+   comments?: IComment[] | null;
+   createdAt?: Timestamp | null | undefined;
+   content?: string | null | undefined;
+   hashtag?: Hashtag | null |undefined;
+   caption? : string | null | undefined;
+   totalTime? : number | null | undefined;
+   ownerGainedTime?: number | null | undefined;
+   listing? : number | null | undefined;
 }
