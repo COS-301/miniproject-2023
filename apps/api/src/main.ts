d3e37f4c60e9caabf0ebedccef8b1e08fbfa4c1e
Tumi <u21452832@tuks.co.za>
2023-03-26 05:41:02 +0000

    a few changes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,1 +1,9 @@
 export * from '@mp/api/core/feature';
+import { NestFactory } from '@nestjs/core';
+import { CoreModule } from '../../../libs/app/core/feature/src/core.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(CoreModule);
+  await app.listen(3000);
+}
+bootstrap();
