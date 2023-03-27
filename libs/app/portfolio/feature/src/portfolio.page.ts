5ba87868c8dd881d5065151f9b138b854d538cb2
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-03-27 22:05:49 +0200

    Integrated portfolio

--- libs/app/portfolio/feature/src/portfolio.page.ts
+++ libs/app/portfolio/feature/src/portfolio.page.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'mp-portfolio',
   templateUrl: './portfolio.page.html',
   styleUrls: ['./portfolio.page.scss'],
 })
 export class PortfolioPage implements OnInit {
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() { }
+
+  toHomePage() {
+    this.router.navigate(["/home"]);
+  }
 }
