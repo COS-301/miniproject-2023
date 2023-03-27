b2c2e86249f214b51136caa35620b7dd48e2d12f
Janco <janco.spies@gmail.com>
2023-03-27 21:40:26 +0200

    Added notifications page

---
+++ libs/app/notifications/feature/src/notifications.page.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'mp-notifications',
+  templateUrl: './notifications.page.html',
+  styleUrls: ['./notifications.page.scss'],
+})
+export class NotificationsPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
