ca4d4c624a32d31c936caffe6e61670fa0f0078f
Michael Ross Tarr <mrmrmtarr@gmail.com>
2023-03-27 23:16:05 +0200

    Firebase Functions FetchPosts

--- libs/api/core/feature/src/functions/feed.function.ts
+++ libs/api/core/feature/src/functions/feed.function.ts
@@ -1,23 +1,15 @@
-// import {  } from '@mp/api/feed/feature';
+
 import { FeedService } from '@mp/api/feed/feature';
-import {
-    FetchPostsRequest,
-    FetchPostsResponse,
-    GetUserTimeRequest,
-    GetUserTimeResponse,
-    AddTimeRequest,
-    AddTimeResponse,
-} from '@mp/api/feed/util';
 import { NestFactory } from '@nestjs/core';
 import * as functions from 'firebase-functions';
 import { CoreModule } from '../core.module';
 
 export const fetchPosts = functions.https.onCall(
-    async (
-      request: any
-    ): Promise<any> => {
-      const app = await NestFactory.createApplicationContext(CoreModule);
-      const service = app.get(FeedService);
-      return service.fetchPosts(request);
-    }
-  );
+  async (
+    request: any
+  ): Promise<any> => {
+    const app = await NestFactory.createApplicationContext(CoreModule);
+    const service = app.get(FeedService, { strict: false });
+    return service.fetchPosts(request);
+  }
+);
--- libs/api/feed/feature/src/feed.service.ts
+++ libs/api/feed/feature/src/feed.service.ts
@@ -1,53 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
-
-import {
-    FetchPostsRequest,
-    FetchPostsResponse,
-    GetUserTimeRequest,
-    GetUserTimeResponse,
-    AddTimeRequest,
-    AddTimeResponse,
-    FetchPostsCommand
-} from '@mp/api/feed/util';
+import { FetchPostsCommand } from '@mp/api/feed/util';
 
 @Injectable()
 export class FeedService {
     constructor(private readonly commandBus: CommandBus) {}
 
-    // fetch posts
-    // get time
-    // add time
-
-    
-    /*
-    * Fetches all of the posts
-    */
     async fetchPosts(
         request: any
     ): Promise<any> {
         return await this.commandBus.execute<
-            any, 
+        FetchPostsCommand, 
             any
-        >(new (request));
+        >(new FetchPostsCommand(request));
     }
-
-    // async getTime(
-    //     reqeust: GetUserTimeRequest
-    // ): Promise<GetUserTimeResponse> {
-    //     return await this.commandBus.execute<
-    //         GetUserTimeCommand,
-    //         GetUserTimeResponse
-    //     >(new GetUserTimeCommand(request));
-    // }
-
-    // async addtime(
-    //     request: AddTimeRequest
-    // ): Promise<AddTimeResponse> {
-    //     return await this.commandBus.execute<
-    //         AddTimeCommand,
-    //         AddTimeResponse
-    //     >(new AddTimeCommand(request));
-    // }
 
 }
