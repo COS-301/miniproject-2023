659a6f73efffb8beeadc884d7cf21984f96892e8
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-03-27 22:13:03 +0200

    Integrated notificaitons page

--- libs/app/notifications/feature/src/notifications.page.ts
+++ libs/app/notifications/feature/src/notifications.page.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'mp-notifications',
   templateUrl: './notifications.page.html',
   styleUrls: ['./notifications.page.scss'],
 })
 export class NotificationsPage implements OnInit {
-  constructor() {}
+  constructor(private router: Router) { }
+
 
   ngOnInit() {}
+
+  toHomePage() {
+    this.router.navigate(["/home"]);
+  }
 }
