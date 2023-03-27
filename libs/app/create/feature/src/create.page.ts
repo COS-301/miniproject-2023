a244229c8962543e527d9ef84990006157164582
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-03-27 21:36:55 +0200

    Implemented create page and fixed home page bar

--- libs/app/create/feature/src/create.page.ts
+++ libs/app/create/feature/src/create.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'mp-create',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.page.scss'],
 })
 export class CreatePage implements OnInit {
-  constructor() {}
+  constructor(private router: Router) { }
 
   ngOnInit() {}
+
+  toDashboard() {
+    this.router.navigate(["/home"]);
+  }
 }
