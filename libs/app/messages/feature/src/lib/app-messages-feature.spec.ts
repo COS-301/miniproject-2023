0edc92c1132f575328830cc73cd2a3d1fb9c8825
Tumi <u21452832@tuks.co.za>
2023-03-28 19:44:15 +0000

    messages boilerplate + non functioning routing

--- libs/app/messages/feature/src/lib/app-messages-feature.spec.ts
+++
@@ -1,7 +0,0 @@
-import { MessagesModule } from './messages.module';
-
-describe('appMessagesFeature', () => {
-  it('should work', () => {
-    expect(new MessagesModule()).toEqual('app-messages-feature');
-  });
-});
--- libs/app/messages/feature/src/lib/messages.module.ts
+++ libs/app/messages/feature/src/lib/messages.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
-// import { MessagesPage } from './messages.page';
+import { MessagesPage } from './messages.page';
 
 
 @NgModule({
   imports: [CommonModule, IonicModule, ],
-  // declarations: [MessagesPage],
+  declarations: [MessagesPage],
 })
 export class MessagesModule {}
