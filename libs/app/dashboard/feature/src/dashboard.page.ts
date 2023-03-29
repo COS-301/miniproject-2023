0fd876c71111045ed8feadad6dbc8a7050a3f872
markbr02 <>
2023-03-29 16:51:13 +0200

    :bug: Fixed major bug for branch

--- libs/app/dashboard/feature/src/dashboard.page.ts
+++ libs/app/dashboard/feature/src/dashboard.page.ts
@@ -4,13 +4,14 @@ import { PostState } from '@mp/app/postss/data-access';
 import { Select } from '@ngxs/store';
 import { Observable } from 'rxjs';
 import { Store } from '@ngxs/store';
-
+import { IProfile } from '@mp/api/profiles/util';
+import { ProfileState } from '@mp/app/profile/data-access';
 @Component({
   selector: 'ms-dashboard-page',
   templateUrl: './dashboard.page.html',
   styleUrls: ['./dashboard.page.scss'],
 })
 export class DashboardPage {
-  @Select(PostState.posts) posts$!: Observable<IPost[]>;
+  @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
 
 }
