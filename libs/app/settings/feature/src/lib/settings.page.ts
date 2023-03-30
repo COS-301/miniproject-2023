6aa41e570283f7a5b609870386908c7d7c72baa4
Jhindosh <dharshanpillay@gmail.com>
2023-03-30 21:36:10 +0200

    Settings page content

--- libs/app/settings/feature/src/lib/settings.page.ts
+++ libs/app/settings/feature/src/lib/settings.page.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'mp-settings',
+  selector: 'app-settings',
   templateUrl: './settings.page.html',
   styleUrls: ['./settings.page.scss']
 })
 export class SettingsPage {
 
+  darkMode = false;
+  notification = true;
+
+  constructor() {
+    //code to do whatever
+  }
+
+  ngOnInit() {
+    // code to initialize settings
+  }
+
+  onSave() {
+    // code to save settings
+  }
 }
