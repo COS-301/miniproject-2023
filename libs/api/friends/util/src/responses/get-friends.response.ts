75cb2f282ee375fcb047816a2dc5e31860c6e2a7
FuryOfD <dharshanng@gmail.com>
2023-04-01 17:29:05 +0200

    get friends function added

---
+++ libs/api/friends/util/src/responses/get-friends.response.ts
@@ -0,0 +1,5 @@
+import { MinimisedProfile } from "../interfaces/minimised-profile.interface";
+
+export interface GetFriendsResponse {
+    profile : MinimisedProfile;
+}
---
+++ libs/api/friends/util/src/responses/index.ts
@@ -0,0 +1,1 @@
+export * from './get-friends.response';
