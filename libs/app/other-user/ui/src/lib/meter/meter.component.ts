c6d6687b5fe3b848291a4b99eff3dfacc83d3fc1
Prometheus18082106 <u21451452@tuks.co.za>
2023-04-02 18:07:29 +0200

    Generated Meter component
    Generated Meter component as a sub component of other-user in the ui folder

---
+++ libs/app/other-user/ui/src/lib/meter/meter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meter',
+  templateUrl: './meter.component.html',
+  styleUrls: ['./meter.component.css']
+})
+export class MeterComponent {
+
+}
