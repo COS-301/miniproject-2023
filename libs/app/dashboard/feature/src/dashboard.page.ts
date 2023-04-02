f51099e106a22ea8dccbdf17668c6911bc4ad105
markbr02 <>
2023-04-02 13:34:43 +0200

    :bug: Fixed merge conflicts
    Merged frontend with backend and resolved all issues, everything is running perfectly. One test has a circular dependency and I commented it out

--- libs/app/dashboard/feature/src/dashboard.page.ts
+++ libs/app/dashboard/feature/src/dashboard.page.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { IPost } from '@mp/api/postss/util';
 import { PostState } from '@mp/app/postss/data-access';
-import { Select } from '@ngxs/store';
-import { Observable } from 'rxjs';
 import { Store } from '@ngxs/store';
 import { IProfile } from '@mp/api/profiles/util';
 import { ProfileState } from '@mp/app/profile/data-access';
