a89a0f0d45b7eb114e8f4ce1cdc3f60c22e20f6d
Prometheus18082106 <u21451452@tuks.co.za>
2023-04-02 17:58:10 +0200

    Created Meter sub component
    Created MeterSubComponent in other-user lib

---
+++ libs/app/other-user/ui/src/lib/meter-sub-component/meter-sub-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meter-sub-component',
+  templateUrl: './meter-sub-component.component.html',
+  styleUrls: ['./meter-sub-component.component.scss']
+})
+export class MeterSubComponentComponent {
+
+}
