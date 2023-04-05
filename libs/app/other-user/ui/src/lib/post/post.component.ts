84fd54740d58a75ae9482b986fe887fbf25af355
Prometheus18082106 <u21451452@tuks.co.za>
2023-04-05 18:33:11 +0200

    Created post template and input
    Create @Input for a post and created html template to display data

--- libs/app/other-user/ui/src/lib/post/post.component.ts
+++ libs/app/other-user/ui/src/lib/post/post.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'post',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./post.component.css']
 })
 export class PostComponent {
-
+  @Input() post: any = {
+    caption: '',
+    // comments: [],
+    imagePath: '',
+  };
 }
