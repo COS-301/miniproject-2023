769d969da8ab920d1e3f603f519a846a80aebacf
Prometheus18082106 <u21451452@tuks.co.za>
2023-04-08 09:59:05 +0200

    Added sub component elements to other user template
    Added necessary imports and exports for post in app-other-user-ui.module
    Added one element each of post, user and badge along with relevant inputs
    Created folder to store assets like images for dev purposes

--- libs/app/other-user/ui/src/lib/app-other-user-ui.module.ts
+++ libs/app/other-user/ui/src/lib/app-other-user-ui.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { MeterComponent, MeterModule } from './meter';
 import { BadgeComponent, BadgeModule } from './badge';
+import { PostComponent, PostModule } from './post';
 // import { ProfileModule } from '@mp/app/profile/data-access';
 // import { FeedOpenModule, FeedOpenComponent } from './feed-open';
 // import { FeedClosedModule, FeedClosedComponent } from './feed-closed';
 @NgModule({
-  imports: [CommonModule, IonicModule, MeterModule, BadgeModule],
+  imports: [CommonModule, IonicModule, MeterModule, BadgeModule, PostModule],
   declarations: [],
-  exports: [MeterComponent, BadgeComponent],
+  exports: [MeterComponent, BadgeComponent, PostComponent],
 })
 export class OtherUserUIModule {}
--- libs/app/other-user/ui/src/lib/post/post.component.ts
+++ libs/app/other-user/ui/src/lib/post/post.component.ts
@@ -3,7 +3,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
   @Input() post: any = {
