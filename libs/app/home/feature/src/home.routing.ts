45dfc09a03f9ad5ee8f2f1d145b1ed5959fc044f
JuliannaVenter <u20433752@tuks.co.za>
2023-04-10 10:56:55 +0200

    Routing fixes to chat
    fixed routing issues from dashboard to chat page and other navigation pages

--- libs/app/home/feature/src/home.routing.ts
+++ libs/app/home/feature/src/home.routing.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
 
+
 const routes: Routes = [
   {
     path: '',
