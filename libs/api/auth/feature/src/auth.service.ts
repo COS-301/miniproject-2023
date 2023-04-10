ec19a4b45266a2061e3941a239cef031b607ac90
ShuaibNuruddin <95973048+ShuaibNuruddin@users.noreply.github.com>
2023-04-10 15:10:51 +0200

    Update auth.service.ts
    Added getId function

--- libs/api/auth/feature/src/auth.service.ts
+++ libs/api/auth/feature/src/auth.service.ts
@@ -2,13 +2,22 @@ import { CreateAuthCommand, ICreateAuthRequest } from '@mp/api/auth/util';
 import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { UserRecord } from 'firebase-admin/auth';
+import {Auth, getAuth} from '@angular/fire/auth'
 
 @Injectable()
 export class AuthService {
+  currentUser: any;
   constructor(private commandBus: CommandBus) {}
 
   onAuthCreate(user: UserRecord) {
     const request: ICreateAuthRequest = { userRecord: user };
     return this.commandBus.execute(new CreateAuthCommand(request));
+  }
+
+  getId(){
+      const auth = getAuth();
+      console.log(auth.currentUser);
+      this.currentUser = auth.currentUser;
+      return this.currentUser?.uid;
   }
 }
