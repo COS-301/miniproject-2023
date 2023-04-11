4305955e25cc01b1253d1cf012f40d6d291b6091
GD Radloff <danielrad082@gmail.com>
2023-04-11 13:25:29 +0200

    fixed authService imports, removed getID()

--- libs/api/auth/feature/src/auth.service.ts
+++ libs/api/auth/feature/src/auth.service.ts
@@ -2,7 +2,6 @@ import { CreateAuthCommand, ICreateAuthRequest } from '@mp/api/auth/util';
 import { Injectable } from '@nestjs/common';
 import { CommandBus } from '@nestjs/cqrs';
 import { UserRecord } from 'firebase-admin/auth';
-import {Auth, getAuth} from '@angular/fire/auth'
 
 @Injectable()
 export class AuthService {
@@ -12,12 +11,5 @@ export class AuthService {
   onAuthCreate(user: UserRecord) {
     const request: ICreateAuthRequest = { userRecord: user };
     return this.commandBus.execute(new CreateAuthCommand(request));
-  }
-
-  getId(){
-      const auth = getAuth();
-      console.log(auth.currentUser);
-      this.currentUser = auth.currentUser;
-      return this.currentUser?.uid;
   }
 }
