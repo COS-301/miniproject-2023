8fae614e244b445d9ad0ea7089e4790a63afbafe
UnarineLeo <unarineleo@gmail.com>
2023-04-11 13:45:47 +0200

    updated the messages page generation

--- libs/app/messages/feature/src/index.ts
+++ libs/app/messages/feature/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/app-messages-feature';
 export * from './lib/messages.module';
 export * from './lib/messages.page';
 export * from './lib/messages.routing';
--- libs/app/messages/feature/src/lib/messages.page.ts
+++ libs/app/messages/feature/src/lib/messages.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatCard } from '../ui-components/chat-card';
 
+
 @Component({
   selector: 'messages-page',
   templateUrl: './messages.page.html',
