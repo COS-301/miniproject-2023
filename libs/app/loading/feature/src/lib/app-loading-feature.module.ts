caee31ca1b689afd3af3a913c61f51b9f8937d31
thapelo <u21499749@tuks.co.za>
2023-04-12 08:24:52 +0200

    Pure html converted to ionic

--- libs/app/loading/feature/src/lib/app-loading-feature.module.ts
+++ libs/app/loading/feature/src/lib/app-loading-feature.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingScreenComponent } from './loading-screen/loading-screen.component';
+import {IonicModule} from "@ionic/angular";
 
 @NgModule({
-  imports: [CommonModule],
+    imports: [CommonModule, IonicModule],
   declarations: [LoadingScreenComponent],
 })
 export class AppLoadingFeatureModule {}
