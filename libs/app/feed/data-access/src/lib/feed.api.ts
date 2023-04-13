8023fdc71a1c671a79fd667a043cf11f49c4365e
Luke Lawson <u21433811@tuks.co.za>
2023-04-13 16:14:44 +0200

    Added functions to feed.api.ts

--- libs/app/feed/data-access/src/lib/feed.api.ts
+++ libs/app/feed/data-access/src/lib/feed.api.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@angular/core';
 import { Firestore } from '@angular/fire/firestore';
 import { Functions, httpsCallable } from '@angular/fire/functions';
 import {
+  AddTimeRequest,
+  AddTimeResponse,
   // FilterList,
   // Post,
   // PostList,
   // TimeModification,
   // UserTime,
   FetchPostsRequest,
-  FetchPostsResponse
+  FetchPostsResponse,
+  GetUserTimeRequest,
+  GetUserTimeResponse
 } from '@mp/api/feed/util';
 
 @Injectable()
@@ -18,11 +22,26 @@ export class FeedApi {
     private readonly functions: Functions
   ) {}
 
-  async fetchPosts$(posts: FetchPostsRequest){
+  async fetchPosts$(request: FetchPostsRequest){
     return await httpsCallable<FetchPostsRequest, FetchPostsResponse>(
       this.functions,
       'fetchPosts'
-    )(posts);
+    )(request);
   }
+
+  async addTime$(request: AddTimeRequest){
+    return await httpsCallable<AddTimeRequest, AddTimeResponse>(
+      this.functions,
+      'addTime'
+    )(request);
+  }
+
+  async getUserTime$(request: GetUserTimeRequest){
+    return await httpsCallable<GetUserTimeRequest, GetUserTimeResponse>(
+      this.functions,
+      'getUserTime'
+    )(request);
+  }
+
 
 }
