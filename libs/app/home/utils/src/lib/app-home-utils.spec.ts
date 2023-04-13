f21eefd3133c6b213eeb5f9f43a94319aacc5b72
08Arno30 <u21457451@tuks.co.za>
2023-04-13 17:16:12 +0200

    Generated data-access and utils libraries for home

---
+++ libs/app/home/utils/src/lib/app-home-utils.spec.ts
@@ -0,0 +1,7 @@
+import { appHomeUtils } from './app-home-utils';
+
+describe('appHomeUtils', () => {
+  it('should work', () => {
+    expect(appHomeUtils()).toEqual('app-home-utils');
+  });
+});
---
+++ libs/app/home/utils/src/lib/app-home-utils.ts
@@ -0,0 +1,3 @@
+export function appHomeUtils(): string {
+  return 'app-home-utils';
+}
