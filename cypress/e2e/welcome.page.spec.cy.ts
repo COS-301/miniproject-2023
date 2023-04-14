172d7627ef7fc6e58c8e9973d254c164e7e0cddd
Marthinus Ettienne van Zyl <u19012366@tuks.co.za>
2023-04-14 09:18:46 +0200

    Improve Welcome Page Cypres Test Structure

--- cypress/e2e/welcome.page.spec.cy.ts
+++ cypress/e2e/welcome.page.spec.cy.ts
@@ -1,35 +1,30 @@
 describe('Welcome Page Test', () => {
 
-  it('Displays Content', () => {
+  beforeEach(() => {
     cy.viewport(390, 844);
     cy.visit('http://localhost:4200/');
+  });
+
+  it('Displays Content', () => {
     cy.get('ion-content').should('be.visible');
   });
 
   it('navigates to login page', () => {
-    cy.viewport(390, 844);
-    cy.visit('http://localhost:4200/');
     cy.get('ion-button').contains('Login').click();
     cy.url().should('include', '/login');
   });
 
   it('Navigates to register page', () => {
-    cy.viewport(390, 844);
-    cy.visit('http://localhost:4200/');
     cy.get('ion-button').contains('Register').click();
     cy.url().should('include', '/register');
   });
 
   it("Navigates to Terms of Service", () => {
-    cy.viewport(390, 844);
-    cy.visit('http://localhost:4200/');
     cy.get('ion-button').contains('Terms of Service').click();
     cy.url().should('include', '/tos');
   });
 
   it("Navigates to Privacy Policy", () => {
-    cy.viewport(390, 844);
-    cy.visit('http://localhost:4200/');
     cy.get('ion-button').contains('Privacy Policy').click();
     cy.url().should('include', '/privacy');
   });
