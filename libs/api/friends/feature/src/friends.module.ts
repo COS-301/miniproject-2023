a4a876fa309f7db33f1e9a172cc64f12db44f3cf
FuryOfD <dharshanng@gmail.com>
2023-04-15 14:59:43 +0200

    completed friends.service and friends.module

--- libs/api/friends/feature/src/friends.module.ts
+++ libs/api/friends/feature/src/friends.module.ts
@@ -1,2 +1,23 @@
 import { Module } from "@nestjs/common";
 import { CqrsModule } from "@nestjs/cqrs";
+import { GetFriendsHandler, RemoveFriendHandler } from "./commands";
+import { FriendsModule as FriendsModuleAccessModule } from "@mp/api/friends/data-access";
+import { FriendsService } from "./friends.service";
+
+
+export const CommandHandlers = [
+    GetFriendsHandler, 
+    RemoveFriendHandler
+];
+
+@Module({
+        imports: [CqrsModule, FriendsModuleAccessModule], 
+        providers: [
+            FriendsService,
+            ...CommandHandlers
+        ],
+        exports: [FriendsService],
+    })
+
+export class FriendsModule{}
+
