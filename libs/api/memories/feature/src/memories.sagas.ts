c0c10ec0948fe48205bf8053bc309d5f31f15ec2
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-04-16 13:58:19 +0200

    memories saga -- implemented

--- libs/api/memories/feature/src/memories.sagas.ts
+++ libs/api/memories/feature/src/memories.sagas.ts
@@ -1,9 +1,15 @@
-import { CreateMemoryCommand } from '@mp/api/memories/util';
+import { CreateMemoryCommand, MemoryCreatedEvent } from '@mp/api/memories/util';
 import { Injectable } from '@nestjs/common';
 import { ICommand, ofType, Saga } from '@nestjs/cqrs';
 import { map, Observable } from 'rxjs';
 
 @Injectable()
 export class MemoriesSagas {
-  // TODO: Add sagas
+  @Saga()
+  onMemoryCreated = (events$: Observable<any>): Observable<ICommand> => {
+    return events$.pipe(
+      ofType(MemoryCreatedEvent),
+      map((event) => new CreateMemoryCommand({ memory: event.memory })),
+    );
+  };
 }
