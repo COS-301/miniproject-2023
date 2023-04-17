fbb9b1a4a35c708fbf90b6543c1b422a4ee2fa6b
Megan Hugo <bellvlekkie@gmail.com>
2023-04-17 14:21:55 +0200

    created feed components

---
+++ libs/app/feed/src/lib/app-feed.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FeedComponent } from './feed/feed.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [FeedComponent],
+})
+export class AppFeedModule {}
---
+++ libs/app/feed/src/lib/feed/feed.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mp-feed',
+  templateUrl: './feed.component.html',
+  styleUrls: ['./feed.component.scss'],
+})
+export class FeedComponent {}
