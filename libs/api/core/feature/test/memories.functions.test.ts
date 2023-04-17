50b79b01bd86ffc6b34ca5c6a3ac4b257222beac
Bandisa <89698022+Bandisa@users.noreply.github.com>
2023-04-17 15:17:59 +0200

    createMemory endpoint test using jest

--- libs/api/core/feature/test/memories.functions.test.ts
+++ libs/api/core/feature/test/memories.functions.test.ts
@@ -1,45 +1,42 @@
 import { describe, test } from '@jest/globals';
-import { createMemory } from '../src';
+import { ICreateMemoryResponse } from '../../../memories/util/src';
 
 const mockData = { //change according to seeded data
-  userId:"0301b908-4fd3-4b2d-af8c-29dd90b4c2fd",
+  userId:"0104fa66-5a7b-429c-aedd-acab833be72e",
   username:"Dessie.Lebsack@yahoo.com",
   title: "Summer Holiday",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-  imgUrl: "https://bit.ly/3MCrcnB" // optional, will be generated
+  imgUrl: "https://bit.ly/3MCrcnB"
 };
 
 describe('Tesing Cloud Function: createMemory', () => {
   test(`Creating Memory for userID = ${mockData.userId}`, async () => {
-    const createMemoryRequest = {
-     data:{
-            memory: {
-            username: mockData.username,
-            userId: mockData.userId,
-            title: mockData.title,
-            description: mockData.description,
-            imgUrl: mockData.imgUrl
-          }
-      }
-    };
-    const memoryResponse = {
-        username: mockData.username,
-        userId: mockData.userId,
-        title: mockData.title,
-        description: mockData.description,
-        imgUrl: mockData.imgUrl
-    };
-    
-    fetch('http://127.0.0.1:5005/demo-project/us-central1/createMemory',{
-      method: 'POST',
-      headers: new Headers({'content-type': 'application/json'}),
-      mode: 'no-cors',
-      body: JSON.stringify(createMemoryRequest)
-    })
-    .then((onSuccess)=>{
-      console.log(onSuccess.body);
-      expect(onSuccess).toBe('')
-    },(e)=>{
-      console.debug(e);
-    })
+      const createMemoryRequest = {
+       data:{
+              memory: {
+              username: mockData.username,
+              userId: mockData.userId,
+              title: mockData.title,
+              description: mockData.description,
+              imgUrl: mockData.imgUrl
+            }
+        }
+      };
+      const memoryResponse = {
+          username: mockData.username,
+          userId: mockData.userId,
+          title: mockData.title,
+          description: mockData.description,
+          imgUrl: mockData.imgUrl
+      };
+      
+      const res = (await fetch('http://127.0.0.1:5005/demo-project/us-central1/createMemory',{
+        method: 'POST',
+        headers: new Headers({'content-type': 'application/json'}),
+        body: JSON.stringify(createMemoryRequest)
+      }))
+      const createMemoryResponse =  (await res.json()).result.ICreateMemoryResponse;
+      console.debug(createMemoryResponse);
+      expect(createMemoryResponse.userId).toBe(mockData.userId);
+    });
 });
