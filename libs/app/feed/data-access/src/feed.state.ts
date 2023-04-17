2e5aa484060a0695993c7e8ceb45c533b6e98e4f
Arno Jooste <91600454+08Arno30@users.noreply.github.com>
2023-04-17 16:06:48 +0200

    State/feed page (#79)
    * Added data access and util libraries to feed
    
    * Implemented possible state for feed
    
    * Updated feed state

---
+++ libs/app/feed/data-access/src/feed.state.ts
@@ -0,0 +1,73 @@
+import { Injectable } from "@angular/core";
+import { Action, Selector, State, StateContext, Store } from '@ngxs/store';
+import { SetError } from '@mp/app/errors/util';
+import produce from 'immer';
+import { FeedApi } from "./feed.api";
+import { SetFeed } from "@mp/app/feed/util";
+import { IGetUserRequest, IUser } from "@mp/api/users/util";
+import { IMemory } from "@mp/api/memories/util";
+
+export interface FeedStateModel {
+    // users: IUser[];
+    memories: IMemory[];
+}
+
+@State<FeedStateModel>({
+    name: 'feed',
+    defaults: {
+        // users: []
+        memories: []
+    },
+})
+
+@Injectable()
+export class FeedState {
+    constructor(
+        private readonly feedApi: FeedApi,
+        private readonly store: Store
+    ){}
+
+    @Selector()
+    static feed(state: FeedStateModel) {
+        return state.memories;
+    }
+
+    // @Action(GetMemoryRequest)
+    // async getUserRequest(ctx: StateContext<FeedStateModel>) {
+    //     try {
+    //         const state = ctx.getState();
+    //         // const _userId = state.users[0]?.userId;
+    //         // const _username = state.users[0]?.username;
+    //         const _memory = state.memories[0];
+
+    //         const request: IGetMemoryRequest = {
+    //             memory: {
+    //                 userId,
+    //                 username,
+    //                 title,
+    //                 description,
+    //                 imgUrl,
+    //                 profileImgUrl,
+    //                 remainingTime,
+    //                 commentsCount,
+    //                 comments,
+    //             }
+    //         }
+    //         const responseRef = await this.feedApi.getFriendsMemories(request);
+    //         const response = responseRef.data;
+    //         return ctx.dispatch(new SetFeed(response.memories));
+    //     }
+    //     catch(error){
+    //         return ctx.dispatch(new SetError((error as Error).message));
+    //     }
+    // }
+
+    // @Action(SetFeed)
+    // setFeed(ctx: StateContext<FeedStateModel>, { memories }: SetFeed) {
+    //     return ctx.setState(
+    //     produce((draft) => {
+    //         draft.memories = memories;
+    //     })
+    //     );
+    // }
+}
---
+++ libs/app/feed/util/src/feed.actions.ts
@@ -0,0 +1,10 @@
+import { IMemory } from "@mp/api/memories/util"
+
+export class GetUserRequest {
+    static readonly type = '[Feed] GetUserRequest';
+}
+
+export class SetFeed {
+    static readonly type = '[Feed] SetFeed';
+    constructor(public readonly memories: IMemory) {}
+}
