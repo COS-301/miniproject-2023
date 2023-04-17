32507c0a24693907a47a1532d3c0336961074f78
AmberLz <u18004874@tuks.co.za>
2023-04-17 19:51:15 +0200

    Added data-access & util libraries

---
+++ libs/app/notifications/util/src/lib/app-notifications-util.spec.ts
@@ -0,0 +1,7 @@
+import { appNotificationsUtil } from './app-notifications-util';
+
+describe('appNotificationsUtil', () => {
+  it('should work', () => {
+    expect(appNotificationsUtil()).toEqual('app-notifications-util');
+  });
+});
