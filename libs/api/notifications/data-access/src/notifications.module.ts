c7a6c8a158d2ea7d584201458780c1ea5d3b44b0
Emito Khoza <emito.khoza@gmail.com>
2023-04-18 21:13:21 +0000

    Created notifications module class, need to create INotification interface, tomorrow's task!

--- libs/api/notifications/data-access/src/notifications.module.ts
+++ libs/api/notifications/data-access/src/notifications.module.ts
@@ -1,7 +1,31 @@
+import { INotification } from '@mp/api/notifications/util';
 import { Injectable } from '@nestjs/common';
+import * as admin from 'firebase-admin';
 
-// @Injectable()
-// export class NotificationsRepository {
+@Injectable()
+export class NotificationRespository {
+    async createNotification(notification: INotification) {
+        return await admin
+        .firestore()
+        .collection('notifications')
+        .doc(notification.id)
+        .create(notification);
+    }
     
-// }
+    async deleteNotification(notification: INotification) {
+        return await admin
+        .firestore()
+        .collection('notifications')
+        .doc(notification.id)
+        .delete();
+    }
+    
+    async getNotifications(userId: string) {
+        return await admin
+        .firestore()
+        .collection('notifications')
+        .where('userId', '==', userId)
+        .get();
+    }
 
+}
