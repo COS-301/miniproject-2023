ae7a8e2c815d6a69b93391d78246410eada24eaf
JulianPienaar <104741835+JulianPienaar@users.noreply.github.com>
2023-04-22 23:14:15 +0200

    Fixed Dashboard to how it was

--- libs/app/dashboard/feature/src/dashboard.page.ts
+++ libs/app/dashboard/feature/src/dashboard.page.ts
@@ -5,7 +5,7 @@ import { IPostDetails, IProfile } from '@mp/api/profiles/util';
 import { ProfileState, ProfileStateModel } from '@mp/app/profile/data-access';
 import { Select } from '@ngxs/store';
 import { Observable, concatMap, filter, map, of, throwError, Subscription, Subject } from 'rxjs';
-import { Router, NavigationExtras } from '@angular/router';
+import { Router } from '@angular/router';
 import { FetchUserPosts, GetAllPosts } from '@mp/app/profile/util';
 import { BuyPost } from '@mp/app/profile/util';
 import { takeUntil } from 'rxjs/operators';
@@ -17,24 +17,15 @@ import { takeUntil } from 'rxjs/operators';
 })
 export class DashboardPage {
   constructor(private router: Router, private store: Store) { }
-  userId: string| null| undefined
   @Select(ProfileState.userPosts) userPosts$: Observable<IPostDetails[]> | undefined;
-  @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
-
 
   ngOnInit() {
-    
     const userId = ' '; // Replace this with the actual user ID
 console.log("here in dispatch");
     this.store.dispatch(new GetAllPosts(userId));
 console.log("we done in dispatch")
-
-    this.profile$.subscribe((profile) =>{
-
-      this.userId = profile?.userId
-    })
   }
-
+  @Select(ProfileState.profile) profile$!: Observable<IProfile | null>;
 
   bid() {
     console.log("bid");
@@ -42,22 +33,8 @@ console.log("we done in dispatch")
   like() {
     console.log("like");
   }
-
-  comment(postId: string| null| undefined) {
-
-    /**
-     * The reason for the snippet below is to get the post id and persist it to the comment page
-     */
-    const navigationExtras: NavigationExtras = {
-      queryParams: {
-        postValueId: postId,
-        userValueId: this.userId
-      }
-    };
-
-    //The persisting happens here
-
-    this.router.navigate(["/comment"], navigationExtras);
+  comment() {
+    this.router.navigate(["/comment"]);
   }
 
 buyPost(i:number){
