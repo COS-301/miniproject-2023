7b49321b053889368c0db75d8384b8eed99c7e25
Stefan van der Merwe <u21429121@tuks.co.za>
2023-04-24 16:18:02 +0000

    Linting fixes, empty ngOnInit(){}

--- libs/app/splash/feature/src/splash.page.ts
+++ libs/app/splash/feature/src/splash.page.ts
@@ -9,7 +9,9 @@ import { Router } from '@angular/router';
 export class SplashPage implements OnInit {
   constructor(private router: Router) { }
 
+  /* eslint-disable */
   ngOnInit() {}
+  /* eslint-enable */
 
   ionViewDidEnter() {
     setTimeout(()=>{
