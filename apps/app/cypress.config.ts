a376d82dec7c51b1678c9837b0bb55506f8d098f
Triumph Ndlovu <realtriumphndlovu@gmail.com>
2023-04-28 05:18:19 +0200

    e2e testing for authentication and welcome page

--- apps/app/cypress.config.ts
+++ apps/app/cypress.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'cypress';
-import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: nxE2EPreset(__dirname, {
-    cypressDir: 'cypress',
-  }),
-});
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    
+  },
+  
+  
+}, );
+
+
