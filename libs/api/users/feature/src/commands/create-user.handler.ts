1e8efefb01c0bde667cb749c29a21eb9ab925788
Armand Krynauw <54016109+ArmandKrynauw@users.noreply.github.com>
2023-04-29 00:20:53 +0200

    Integration/authentication (#141)
    * Remove temp files and directories
    
    * Add default username on register
    
    Clean up login page styling

--- libs/api/users/feature/src/commands/create-user.handler.ts
+++ libs/api/users/feature/src/commands/create-user.handler.ts
@@ -1,29 +1,48 @@
 import { CreateUserCommand, IUser } from '@mp/api/users/util';
 import { CommandHandler, EventPublisher, ICommandHandler } from '@nestjs/cqrs';
 import { User } from '../models';
+import { UsersRepository } from '@mp/api/users/data-access';
 
 @CommandHandler(CreateUserCommand)
 export class CreateUserHandler implements ICommandHandler<CreateUserCommand> {
-  constructor(private publisher: EventPublisher) {}
+  constructor(
+    private publisher: EventPublisher,
+    private userRepository: UsersRepository
+  ) {}
 
   async execute(command: CreateUserCommand) {
     console.log(`${CreateUserHandler.name}`);
+
+    let username = '';
+    let i = 0;
+
+    while (i++ < 10) {
+      username = generateRandomUsername();
+      const snapshot = await this.userRepository.findUserWithUsername(username);
+
+      if (snapshot.empty)
+        break;
+    }
 
     const request = command.request;
     const data: IUser = {
       userId: request.auth.id,
       name:"",
       surname:"",
-      username:request.auth.displayName,
+      username: username,
       email: request.auth.email,
       profileImgUrl: request.auth.photoURL,
       created: request.auth.created,
-      //time: 120.0,
-      
     };
     const user = this.publisher.mergeObjectContext(User.fromData(data));
 
     user.create();
     user.commit();
   }
+}
+
+function generateRandomUsername(): string {
+  const prefix = "user";
+  const randomString = Math.random().toString(36).substring(2, 7);
+  return `${prefix}-${randomString}`;
 }
