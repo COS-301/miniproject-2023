b0c536e4597437b44a5dc3672e955e0703c57233
AlistairMRoss <alistairmikeross@gmail.com>
2023-04-29 10:18:54 +0200

    Trying to fix unit tests, need to way for changes to firestore config

--- libs/app/shared/feature/src/lib/memory-card/memory-card.component.spec.ts
+++ libs/app/shared/feature/src/lib/memory-card/memory-card.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { MemoryCardComponent } from './memory-card.component';
+import { Store, NgxsModule } from '@ngxs/store';
+import { MemoryCardApi, MemoryCardState } from '@mp/app/shared/data-access';
 
 describe('MemoryCardComponent', () => {
   let component: MemoryCardComponent;
@@ -9,6 +11,8 @@ describe('MemoryCardComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MemoryCardComponent],
+      imports: [NgxsModule.forRoot([MemoryCardState])],
+      providers: [Store, MemoryCardApi],
       schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
     }).compileComponents();
 
